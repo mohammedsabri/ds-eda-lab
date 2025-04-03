91b284be77b6a6a013a619cb657a99b13575b392
mohammed sabri <mohammedsabri228@gmail.com>
2025-04-03 20:36:40 +0100

    Completed Fan out implementation - added emailing logic

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SES_REGION = "eu-west-1";
+export const SES_EMAIL_FROM = "20113428@mail.wit.ie";
+export const SES_EMAIL_TO =  "mohammedsabri228@gmail.com";
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -23,15 +23,16 @@ export class EDAAppStack extends cdk.Stack {
         });
 
         const imageProcessQueue = new sqs.Queue(this, "img-created-queue", {
-          receiveMessageWaitTime: cdk.Duration.seconds(10),
+            receiveMessageWaitTime: cdk.Duration.seconds(5),
         });
-    
+
+        const mailerQ = new sqs.Queue(this, "mailer-queue", {
+            receiveMessageWaitTime: cdk.Duration.seconds(10),
+        });
+
         const newImageTopic = new sns.Topic(this, "NewImageTopic", {
-          displayName: "New Image topic",
-      });
-
-
-
+            displayName: "New Image topic",
+        });
 
         // Lambda functions
 
@@ -46,26 +47,63 @@ export class EDAAppStack extends cdk.Stack {
             }
         );
 
-        // S3 --> SQS
+        const mailerFn = new lambdanode.NodejsFunction(
+            this,
+            "mailer-function",
+            {
+                runtime: lambda.Runtime.NODEJS_16_X,
+                memorySize: 1024,
+                timeout: cdk.Duration.seconds(3),
+                entry: `${__dirname}/../lambdas/mailer.ts`,
+            }
+        );
+
+        // S3 --> SQS (make queue a subscriber to the SNS topic)
         imagesBucket.addEventNotification(
             s3.EventType.OBJECT_CREATED,
-            new s3n.SqsDestination(imageProcessQueue)
+            new s3n.SnsDestination(newImageTopic)
         );
 
         newImageTopic.addSubscription(
-          new subs.SqsSubscription(imageProcessQueue)
-      );
-        // SQS --> Lambda
-        const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
+            new subs.SqsSubscription(imageProcessQueue)
+        );
+
+        newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
+
+        // SQS --> Lambda (event source from the SQS queues)
+        const newImageEventSource = new events.SqsEventSource(
+            imageProcessQueue,
+            {
+                batchSize: 5,
+                maxBatchingWindow: cdk.Duration.seconds(5),
+            }
+        );
+
+        const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
             batchSize: 5,
             maxBatchingWindow: cdk.Duration.seconds(5),
         });
 
+        //  triggers for the lambda functions
         processImageFn.addEventSource(newImageEventSource);
+
+        mailerFn.addEventSource(newImageMailEventSource);
 
         // Permissions
 
         imagesBucket.grantRead(processImageFn);
+
+        mailerFn.addToRolePolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ses:SendEmail",
+                    "ses:SendRawEmail",
+                    "ses:SendTemplatedEmail",
+                ],
+                resources: ["*"],
+            })
+        );
 
         // Output
 
